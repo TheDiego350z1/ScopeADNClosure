623d39715dbdc48ca0f6988d02ccfc012f70a82d
Diego Garcia <diego350z1@gmail.com>
2021-07-21 16:26:29 -0600

    closure

---
+++ src/closure/closure.js
@@ -0,0 +1,23 @@
+const moneyBox = (coins) => {
+    var saveCoins = 0;
+    saveCoins += coins;
+    console.log(`MoneBox: $${saveCoins}`);
+}
+
+moneyBox(5);
+moneyBox(10);
+
+const monetBox = () => {
+    var saveCoins = 0;
+    const countCoins = (coins) => {
+        saveCoins += coins;
+        console.log (`MonetBox: $${saveCoins}`);
+    }
+    return countCoins;
+}
+
+let myMoneyBox = monetBox();
+
+myMoneyBox(4);
+myMoneyBox(6);
+myMoneyBox(10);
